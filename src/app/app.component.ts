d5ca2b927054cc0cef90b2fcf99a10d287289d4d
Lucas Dias <lucas.dortiz17@gmail.com>
2024-11-17 16:27:53 -0300

    created components from blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
