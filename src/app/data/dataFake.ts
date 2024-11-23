65c8dae50a57fd5772a384d4c0bd0be904128e4f
Lucas Dias <lucas.dortiz17@gmail.com>
2024-11-23 02:12:02 -0300

    adjusted title and description from content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,30 +1,18 @@
 export const dataFake = [
 	{
-		id: "1",
-		photo: 'https://i.em.com.br/mlNy6lHlVyE6VLUtR7_8uqSlMqg=/1200x675/smart/imgsapp.em.com.br/app/noticia_127983242361/2023/04/17/1482152/personagens-peni-parker-spider-gwin-peter-porker-miles-morales-peter-parker-e-spider-man-noir-protagonistas-da-animacao-homem-aranha-atraves-do-aranhaverso-parte-um_1_101261.jpg',
+		id: '1',
+		photo:
+			'https://i.em.com.br/mlNy6lHlVyE6VLUtR7_8uqSlMqg=/1200x675/smart/imgsapp.em.com.br/app/noticia_127983242361/2023/04/17/1482152/personagens-peni-parker-spider-gwin-peter-porker-miles-morales-peter-parker-e-spider-man-noir-protagonistas-da-animacao-homem-aranha-atraves-do-aranhaverso-parte-um_1_101261.jpg',
 		title: 'Filme Homem Aranha',
 		description:
-			'Lorem ipsum dolor sit amet. 33 temporibus voluptate ab necessitatibus quae et perferendis iusto aut quos velit? Non voluptatem repellendus et libero totam aut modi quia in impedit nobis. Et doloremque autem nam voluptatum provident aut aperiam blanditiis et voluptatem voluptatibus sit repellendus praesentium in labore laborum.',
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore minus harum tempora, ipsam, ab laborum cum cumque numquam impedit nulla veritatis magni hic cupiditate vero debitis commodi, optio neque sequi?',
 	},
 	{
-		id: "2",
-		photo: 'link',
-		title: 'Filme Homem Aranha',
+		id: '2',
+		photo:
+			'https://community.softr.io/uploads/db9110/original/2X/7/74e6e7e382d0ff5d7773ca9a87e6f6f8817a68a6.jpeg',
+		title: 'Example',
 		description:
-			'Lorem ipsum dolor sit amet. 33 temporibus voluptate ab necessitatibus quae et perferendis iusto aut quos velit? Non voluptatem repellendus et libero totam aut modi quia in impedit nobis. Et doloremque autem nam voluptatum provident aut aperiam blanditiis et voluptatem voluptatibus sit repellendus praesentium in labore laborum.',
-	},
-	{
-		id: "3",
-		photo: 'link',
-		title: 'Filme Homem Aranha',
-		description:
-			'Lorem ipsum dolor sit amet. 33 temporibus voluptate ab necessitatibus quae et perferendis iusto aut quos velit? Non voluptatem repellendus et libero totam aut modi quia in impedit nobis. Et doloremque autem nam voluptatum provident aut aperiam blanditiis et voluptatem voluptatibus sit repellendus praesentium in labore laborum.',
-	},
-	{
-		id: "4",
-		photo: 'link',
-		title: 'Filme Homem Aranha',
-		description:
-			'Lorem ipsum dolor sit amet. 33 temporibus voluptate ab necessitatibus quae et perferendis iusto aut quos velit? Non voluptatem repellendus et libero totam aut modi quia in impedit nobis. Et doloremque autem nam voluptatum provident aut aperiam blanditiis et voluptatem voluptatibus sit repellendus praesentium in labore laborum.',
+			'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque unde ex ipsam! Optio cum, debitis itaque unde vitae, nisi culpa animi porro sed quas explicabo quibusdam soluta officia eligendi necessitatibus.',
 	},
 ];
